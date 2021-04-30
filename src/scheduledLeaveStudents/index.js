101b2c9cbd6f7b8124d70a3ece30b6c64f9b50ed
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-04-30 19:53:54 +0900

    Add function

--- src/scheduledLeaveStudents/index.js
+++ src/scheduledLeaveStudents/index.js
@@ -1,48 +1,48 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const dynamoDB = new AWS.DynamoDB.DocumentClient({
-    region: process.env.DYNAMODB_REGION,
+  region: process.env.DYNAMODB_REGION,
 });
 
 // ハンドラー関数
 exports.handler = function (event, context, callback) {
-    const params = {
-        TableName: process.env.DYNAMODB_TABLE,
-    };
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+  };
 
-    dynamoDB.scan(params, function (err, data) {
-        if (err) {
-            console.log('Error:', err);
-        } else {
-            const students = data.Items;
-            leaveAllStudents(students);
-        }
-    });
+  dynamoDB.scan(params, function (err, data) {
+    if (err) {
+      console.log("Error:", err);
+    } else {
+      const students = data.Items;
+      leaveAllStudents(students);
+    }
+  });
 };
 
-function leaveAllStudents (students) {
-    students.forEach(student => {
-        leaveStudent(student);
-    });
+function leaveAllStudents(students) {
+  students.forEach((student) => {
+    leaveStudent(student);
+  });
 }
 
-function leaveStudent (student) {
-    if (student.is_stay) {
-        const params = {
-            TableName: process.env.DYNAMODB_TABLE,
-            Key: {
-                'id': student.id
-            },
-            UpdateExpression: 'set is_stay = :v',
-            ExpressionAttributeValues: {
-                ':v': false
-            },
-            ReturnValues: 'UPDATED_NEW'
-        };
-        
-        dynamoDB.update(params, function(err, data) {
-            if (err) {
-                console.log('Error:', err);
-            }
-        });
-    }
+function leaveStudent(student) {
+  if (student.is_stay) {
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Key: {
+        id: student.id,
+      },
+      UpdateExpression: "set is_stay = :v",
+      ExpressionAttributeValues: {
+        ":v": false,
+      },
+      ReturnValues: "UPDATED_NEW",
+    };
+
+    dynamoDB.update(params, function (err, data) {
+      if (err) {
+        console.log("Error:", err);
+      }
+    });
+  }
 }
