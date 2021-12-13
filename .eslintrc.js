6a75788f50fd474d9a987c1448e453604533a943
kogepanh <k.hosono1209@gmail.com>
2021-12-13 18:09:13 +0900

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,12 @@
 module.exports = {
   env: {
-    browser: true,
     commonjs: true,
     node: true,
-    es6: true,
   },
   parserOptions: {
     sourceType: "module", // Default script
-    ecmaVersion: 2021,
+    ecmaVersion: "latest",
   },
-  plugins: ["prettier"],
   extends: ["eslint:recommended", "prettier"],
-  rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        printWidth: 150,
-        singleQuote: false,
-      },
-    ],
-  },
+  rules: {},
 };
