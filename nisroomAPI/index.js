4cd5f5eba74f48e7c6bd56e9be74b37c1d6b9266
kogepanh <k.hosono1209@gmail.com>
2022-01-22 09:56:34 +0900

    update

---
+++ nisroomAPI/index.js
@@ -0,0 +1,25 @@
+const AWS = require("aws-sdk");
+
+const Users = require("users");
+
+AWS.config.update({
+  region: "ap-northeast-1",
+  endpoint: "dynamodb.ap-northeast-1.amazonaws.com",
+});
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context, callback) => {
+  console.log("event:", event);
+  if (event.httpMethod === "GET" && event.pathParameters.proxy === "users") {
+    if (!event.queryStringParameters) {
+      Users();
+    }
+  }
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+  return response;
+};
