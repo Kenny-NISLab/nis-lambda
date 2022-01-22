38fd839c50546bad7d66d77e3dd83e8c716d0abe
kogepanh <k.hosono1209@gmail.com>
2022-01-22 13:13:40 +0900

    update

--- nisroomAPI/index.js
+++ nisroomAPI/index.js
@@ -1,10 +1,10 @@
 const getUsers = require("./nisroom/getUsers");
 const getUser = require("./nisroom/getUser");
-// const patchUser = require("./nisroom/patchUser");
+const patchUser = require("./nisroom/patchUser");
 
 exports.handler = async (event, context, callback) => {
   event.path = decodeURI(event.path).split("/").filter(Boolean); // パス内に日本語が存在する可能性があるのでデコード後，パスをパースし，空文字を削除して整形
-  console.log("event:", event); ////////////////////////////////////////////////////////
+  event.body = JSON.parse(event.body);
 
   if (event.path[0] === "users") {
     switch (event.path.length) {
@@ -18,8 +18,7 @@ exports.handler = async (event, context, callback) => {
         if (event.httpMethod === "GET") {
           await getUser(event, callback);
         } else if (event.httpMethod === "PATCH") {
-          console.log(event.body);
-          // await patchUser(event, callback);
+          await patchUser(event, callback);
         }
         break;
       }
