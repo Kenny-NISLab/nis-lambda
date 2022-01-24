e88716e68165995500709bf37f8cca8b05c075cd
kogepanh <k.hosono1209@gmail.com>
2022-01-24 15:23:33 +0900

    update

--- inoutNISRoom/index.js
+++ inoutNISRoom/index.js
@@ -1,5 +1,6 @@
 const AWS = require("aws-sdk");
 const moment = require("moment");
+const Converter = AWS.DynamoDB.Converter;
 
 AWS.config.update({
   region: "ap-northeast-1",
@@ -12,46 +13,69 @@ exports.handler = (event, context, callback) => {
   // 変更イベントが配列として渡される
   event.Records.map((record) => {
     if (record.eventName === "MODIFY") {
-      const OldImage = record.dynamodb.OldImage;
-      const NewImage = record.dynamodb.NewImage;
+      // DynamoDB JSON を通常のオブジェクトに変換
+      const OldImage = Converter.unmarshall(record.dynamodb.OldImage);
+      const NewImage = Converter.unmarshall(record.dynamodb.NewImage);
 
+      // 在室情報が更新された場合
       if (OldImage.isStay !== NewImage.isStay) {
-        console.log("DynamoDB Modify:", OldImage, NewImage);
+        const today = moment().utcOffset(9).format("YYYY-MM-DD");
 
-        // const now = Date.parse(new Date()).toString(10);
+        // 入室した場合
+        if (NewImage.isStay) {
+          // すでにその日付のオブジェクトが存在する場合
+          if (NewImage.stayLab[today]) {
+            NewImage.stayLab[today]["inAt"] = moment().utcOffset(9).format();
+          } else {
+            NewImage.stayLab[today] = { inAt: moment().utcOffset(9).format() };
+          }
 
-        // let inout = null;
-        // if(newItem.is_stay.BOOL){
-        //   inout = 'in';
-        // }else{
-        //   inout = 'out';
-        // }
+          // 退室した場合
+        } else {
+          // すでにその日付のオブジェクトが存在する場合（その日に入室した場合が殆ど）
+          if (NewImage.stayLab[today]) {
+            // その日に入室している場合
+            if (NewImage.stayLab[today]["inAt"]) {
+              NewImage.stayLab[today]["outAt"] = moment().utcOffset(9).format(); // 退室時間
 
-        // const item = {
-        //   'id': newItem.id,
-        //   'created_at': {N: now},
-        //   'j_full_name': newItem.j_full_name,
-        //   'e_full_name': newItem.e_full_name,
-        //   'inout': {S: inout},
-        // };
+              const diffTime = moment().utcOffset(9).diff(moment(NewImage.stayLab[today]["inAt"])); // その日の入室時間との差（ms）を計算
 
-        // dynamodbPut(item);
+              // すでにその日に一度退室した場合
+              if (NewImage.stayLab[today]["stayTime"]) {
+                NewImage.stayLab[today]["stayTime"] += diffTime / 1000; // すでにある時間に加算
+              } else {
+                NewImage.stayLab[today]["stayTime"] = diffTime / 1000;
+              }
+            } else {
+              return;
+            }
+          } else {
+            return;
+          }
+        }
+
+        updateDynamoDB(NewImage);
       }
     }
   });
 };
 
-function dynamodbPut(item) {
+function updateDynamoDB(Image) {
   const params = {
-    TableName: process.env.DYNAMODB_TABLE,
-    Item: item,
+    TableName: "Students",
+    Key: {
+      name: Image.name,
+    },
+    UpdateExpression: "set stayLab = :stayLab",
+    ExpressionAttributeValues: {
+      ":stayLab": Image.stayLab,
+    },
+    ReturnValues: "NONE",
   };
 
-  docClient.putItem(params, function (err, data) {
+  docClient.update(params, (err, data) => {
     if (err) {
-      console.log("Error:", err);
-    } else {
-      console.log("Success:", data);
+      console.error(err);
     }
   });
 }
